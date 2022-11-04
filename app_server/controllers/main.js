8028574db754da9c7655c787068ce1c3e025ec98
Jordan Davis <jordand@Jordans-MBP.windstream.net>
2022-11-04 02:08:24 -0400

    Express website, static HTML only

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,6 @@
 /* GET Homepage */
 const index = (req, res) => {
-    pageTitle = process.env.npm_package_description;
+    pageTitle = process.env.npm_package_description + ' - Main';
     res.render('index', { title: pageTitle });
 };
 module.exports = {
